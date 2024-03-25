967474b24f1e7901ce057ca6d3f6297d6d736064
futurebound <mitchellm1618@gmail.com>
2024-03-25 16:31:44 -0700

    feat: form submit updates with new books, toggle read status

--- projects/03-library/script.js
+++ projects/03-library/script.js
@@ -10,6 +10,9 @@ function Book(title, author, pages, read) {
   this.info = () => {
     return `${this.title} by ${this.author}, ${pages} pages, ${this.read ? "has been read" : "not read yet"}`;
   }
+  this.toggleRead = () => {
+    this.read = !this.read;
+  }
 }
 
 function addBookToLibrary(book) {
@@ -18,16 +21,17 @@ function addBookToLibrary(book) {
 
 function removeBookByIndex(index) {
   myLibrary.splice(index, 1)
-  // console.log(`removed ${title} from library`)
   console.log(myLibrary)
 }
 
-function createNewBook() {
-
+function toggleReadByIndex(index) {
+  myLibrary[index].toggleRead()
 }
 
 function displayAllBooks() {
   const container = document.querySelector(".container");
+  container.replaceChildren()
+
   for (let i = 0; i < myLibrary.length; i++) {
     const book = myLibrary[i]
 
@@ -43,29 +47,58 @@ function displayAllBooks() {
     author.textContent = book.author
     card.appendChild(author)
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.setAttribute("class", "deleteBtn")
-    deleteBtn.addEventListener("click", (e) => {
-      // console.log(e.target.parentNode)
+    const toggleReadBtn = document.createElement("button")
+    toggleReadBtn.setAttribute("class", "toggle-button")
+    toggleReadBtn.addEventListener("click", (e) => {
       const parentBook = e.target.parentNode
-      // console.log(parentBook.id)
+      toggleReadByIndex(parseInt(parentBook.id))
+      displayAllBooks()
+    })
+    toggleReadBtn.textContent = "TOGGLE READ STATUS"
+    card.appendChild(toggleReadBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.setAttribute("class", "delete-button")
+    deleteBtn.addEventListener("click", (e) => {
+      const parentBook = e.target.parentNode
       removeBookByIndex(parseInt(parentBook.id))
-      container.removeChild(parentBook)
-      container.replaceChildren()
       displayAllBooks()
     })
     deleteBtn.textContent = "DELETE"
     card.appendChild(deleteBtn)
 
+    if (book.read) {
+      card.classList.add("read")
+    } else {
+      card.classList.add("not-read")
+    }
 
     container.appendChild(card);
   }
 }
 
-const newBookBtn = document.querySelector(".new-book")
-newBookBtn.addEventListener("click", () => {
-  // populate form
-  
+const modal = document.querySelector(".modal")
+const openModal = document.querySelector(".open-button")
+const closeModal = document.querySelector(".close-button")
+const submitModal = document.querySelector(".submit-button")
+
+openModal.addEventListener("click", () => {
+  modal.showModal()
+})
+
+submitModal.addEventListener("click", (e) => {
+  e.preventDefault()
+  let book = {}
+  const data = new FormData(document.querySelector("#modal-form"))
+  for (const [field, value] of data.entries()) {
+    console.log(field, value);
+    book[field] = value
+  }
+  const newBook = new Book(book.title, book.author, book.pages, 
+      book.read === "on" ? true : false)
+  addBookToLibrary(newBook)
+  displayAllBooks()
+  modal.close()
 })
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
