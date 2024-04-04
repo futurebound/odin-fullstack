971c9ba9fcbddd13f84a9b7458ccb8811e436b42
futurebound <mitchellm1618@gmail.com>
2024-04-03 17:27:33 -0700

    feat(todo-app): skeleton HTML, JS and CSS
    simple app layout of sidebar and content sections

--- projects/06-todo-list/src/index.js
+++ projects/06-todo-list/src/index.js
@@ -6,3 +6,24 @@ if (process.env.NODE_ENV !== 'production') {
 } else if (process.env.NODE_ENV === 'production') {
   console.log('We are in production mode!');
 }
+
+const containerDiv = document.getElementById("container");
+// const title = document.createElement("h1");
+// title.textContent = "Welcome to Todo App";
+// containerDiv.appendChild(title);
+
+const sidebarDiv = document.createElement("div");
+sidebarDiv.classList.add("sidebar");
+const sidebarTitle = document.createElement("h1");
+sidebarTitle.textContent = "Sidebar";
+sidebarDiv.appendChild(sidebarTitle);
+
+const contentDiv = document.createElement("div");
+contentDiv.classList.add("content");
+const contentTitle = document.createElement("h1");
+contentTitle.textContent = "Content";
+contentDiv.appendChild(contentTitle);
+
+
+containerDiv.appendChild(sidebarDiv);
+containerDiv.appendChild(contentDiv);
