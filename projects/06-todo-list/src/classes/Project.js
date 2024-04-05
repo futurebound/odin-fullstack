880bf7aa7cd79a094ddb025ec1d4d495ed182132
futurebound <mitchellm1618@gmail.com>
2024-04-05 13:15:18 -0700

    feat(todo-app): added Task and Project data model classes

---
+++ projects/06-todo-list/src/classes/Project.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Project {
+  constructor(title, description, tasks=[]) {
+    this.title = title;
+    this.description = description;
+    this.tasks = tasks;
+  }
+
+  
+}
+
+export default Project;
---
+++ projects/06-todo-list/src/classes/Task.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Task {
+  constructor(title, description, dueDate, priority, project, completed) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+    this.completed = completed;
+    // this.subtasks = [];
+  }
+
+  
+}
+
+export default Task;
