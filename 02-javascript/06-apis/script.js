01dd36b84b6d6819cbbfc0cf4158c5acc934e3ed
futurebound <mitchellm1618@gmail.com>
2024-04-16 14:06:18 -0700

    feat(apis): alert and re-prompting for invalid api key, custom image query

--- 02-javascript/06-apis/script.js
+++ 02-javascript/06-apis/script.js
@@ -3,15 +3,32 @@
 // require("dotenv").config();
 // const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
 
-const apiKey = prompt("enter your GIPHY API key")
+let GIPHY_API_KEY = prompt("enter your GIPHY API key");
 
 const img = document.querySelector('img');
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=cats`, { mode: 'cors' })
-  .then(function (response) {
-    // console.log(response.json());
-    return response.json();
-  })
-  .then(function(response) {
-    // console.log(response.data.images.original.url)
-    img.src = response.data.images.original.url;
-  });
+const imgButton = document.getElementById("img-button");
+const searchInput = document.getElementById("search");
+
+imgButton.addEventListener("click", () => {
+  const queryTerm = searchInput.value;
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${queryTerm}`, { mode: 'cors' })
+    .then((response) => {
+      if (!response.ok) {
+        alert("invalid API key");
+        GIPHY_API_KEY = prompt("enter your GIPHY API key");
+      }
+      // console.log(response.json());
+      return response.json();
+    })
+    // .catch((error) => {
+    //   console.error(error);
+    // })
+    .then((response) => {
+      if (response.data.length <= 0) {
+        alert("no matching images found");
+      } else {
+        img.src = response.data.images.original.url;
+      } 
+    });
+});
+
