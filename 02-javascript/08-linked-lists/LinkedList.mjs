6ae00c53faa5c02c5cbe8582b8ffeb671d9a065e
futurebound <mitchellm1618@gmail.com>
2024-04-23 15:15:43 -0700

    feat(linked-lists): append node to list

--- 02-javascript/08-linked-lists/LinkedList.mjs
+++ 02-javascript/08-linked-lists/LinkedList.mjs
@@ -4,8 +4,37 @@ import Node from './Node.mjs';
 
 class LinkedList {
   constructor() {
-    this.head = new Node();
-    this.tail = this.head;
+    this.head = null;
+    this.tail = null;
     this.size = 0;
   }
+
+  // O(1)
+  append(value) {
+    const node = new Node(value);
+
+    if (this.head === null) this.head = node;
+    this.tail = node;
+    this.size++;
+  }
+
+  prepend(value) {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
 }
+
+export default LinkedList;
