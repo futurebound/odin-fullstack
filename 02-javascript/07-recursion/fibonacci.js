5ce682507cccfe990589f4b4e440fb29850ceffc
futurebound <mitchellm1618@gmail.com>
2024-04-23 09:19:44 -0700

    feat(recursion): recursive fibonacci solution

--- 02-javascript/07-recursion/fibonacci.js
+++ 02-javascript/07-recursion/fibonacci.js
@@ -20,9 +20,41 @@ const fibs = (n) => {
   return output;
 };
 
-console.log(fibs(-1));
-console.log(fibs(0));
-console.log(fibs(1));
-console.log(fibs(2));
-console.log(fibs(3));
-console.log(fibs(8));
+// console.log('Testing iterative fibs():');
+// console.log(fibs(-1));
+// console.log(fibs(0));
+// console.log(fibs(1));
+// console.log(fibs(2));
+// console.log(fibs(3));
+// console.log(fibs(8));
+
+const fibsRec = (n) => {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  }
+
+  let output = [0, 1];
+  for (let i = 2; i < n; i++) {
+    output.push(fibsRecHelper(i));
+  }
+  return output;
+};
+
+const fibsRecHelper = (n) => {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fibsRecHelper(n - 1) + fibsRecHelper(n - 2);
+};
+
+console.log('Testing recursive fibsRec():');
+console.log(fibsRec(-1));
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(3));
+console.log(fibsRec(8));
