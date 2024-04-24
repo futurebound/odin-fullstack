d9b47f5379ec1034dfb39cd7bd0d5838435369c4
futurebound <mitchellm1618@gmail.com>
2024-04-23 17:03:50 -0700

    feat(linked-lists): tests for LinkedList
    prepend, size, head, and tail

--- 02-javascript/08-linked-lists/test.mjs
+++ 02-javascript/08-linked-lists/test.mjs
@@ -4,7 +4,6 @@ import LinkedList from './LinkedList.mjs';
 
 const testAppend = () => {
   const list = new LinkedList();
-
   list.append(1);
   console.log(list.toString());
   list.append(2);
@@ -13,4 +12,55 @@ const testAppend = () => {
   console.log(list.toString());
 };
 
-testAppend();
+const testPrepend = () => {
+  const list = new LinkedList();
+  list.prepend(1);
+  console.log(list.toString());
+  list.prepend(2);
+  console.log(list.toString());
+  list.prepend(3);
+  console.log(list.toString());
+};
+
+const testSize = () => {
+  const list = new LinkedList();
+  console.log(list.size);
+  list.append(1);
+  console.log(list.size);
+  list.append(2);
+  console.log(list.size);
+};
+
+const testHead = () => {
+  const list = new LinkedList();
+  console.log(list.head);
+  list.append(1);
+  console.log(list.head);
+  list.append(2);
+  console.log(list.head);
+};
+
+const testTail = () => {
+  const list = new LinkedList();
+  console.log(list.tail);
+  list.append(1);
+  console.log(list.tail);
+  list.append(2);
+  console.log(list.tail);
+};
+
+const testAt = () => {
+  const list = new LinkedList();
+  list.append(1);
+  list.append(2);
+  console.log(list.at(0));
+  console.log(list.at(1));
+  console.log(list.at(2));
+};
+
+// testAppend();
+// testPrepend();
+// testSize();
+// testHead();
+// testTail();
+testAt();
