d195b132750aef75beeef6ca09e8e784d8f62964
futurebound <mitchellm1618@gmail.com>
2024-05-03 10:59:41 -0700

    feat(knights-travails): knightMoves solution found
    for BFS, to track path travelled only ever put entire paths into queue

--- projects/07-knights-travails/knightMoves.mjs
+++ projects/07-knights-travails/knightMoves.mjs
@@ -41,95 +41,32 @@ class KnightMoves {
       }
     }
 
-    // console.log(legalMovesMap);
     return legalMovesMap;
   }
 
   knightMoves(origin, target) {
-    console.log(`origin: ${origin}, target: ${target}`);
-
     const queue = [];
-    let pathsTravelled = [];
-    const visited = new Set();
-    const solutions = new Set();
+    queue.push([origin]);
 
-    // initial path of [[origin]]
-    pathsTravelled.push([origin]);
-    visited.add(JSON.stringify(origin));
-
-    let legalMoveSet = this.movesMap.get(JSON.stringify(origin));
-    legalMoveSet.forEach((move) => {
-      queue.push(move);
-    });
-
-    let numSolutionMoves = 1; // think this is redundant
-    let numMovesToTest = queue.length;
-
-    // with BFS, can more easily track how many moves made (levelSize)
-    //  i.e. per level, queue contains ALL possible moves we need to check that
-    //  are "levelSize" away from target
-    // only pop out "levelSize" moves from queue at a time
-    //  check if they are viable solutions, if so add them
-    // TODO: need like a PATH array -> something to add to solutions Set
-    while (solutions.size <= 0) {
-      for (let i = 0; i < numMovesToTest; i++) {
-        const current = queue.shift();
-        visited.add(JSON.stringify(current));
-        pathsTravelled = this.updatePaths(pathsTravelled, current, visited);
-
-        if (JSON.stringify(current) === JSON.stringify(target)) {
-          // iterate through pathsTravelled
-          //  get path travelled, check for last index being === target
-          //  once found, return that path
-          pathsTravelled.forEach((path) => {
-            if (
-              JSON.stringify(path[path.length - 1]) === JSON.stringify(target)
-            ) {
-              console.log(path);
-            }
-          });
-          solutions.add('found it baybe');
-          break;
-        }
-
-        // console.log(legalMoveSet);
-        legalMoveSet = this.movesMap.get(JSON.stringify(current));
-        legalMoveSet.forEach((move) => {
-          if (!visited.has(JSON.stringify(move))) {
-            queue.push(move);
-          }
-        });
+    while (queue.length > 0) {
+      const currentPath = queue.shift();
+      const currentLocation = currentPath[currentPath.length - 1];
+      if (JSON.stringify(currentLocation) === JSON.stringify(target)) {
+        console.log(
+          `You made it in ${currentPath.length - 1} moves! Here's your path:`
+        );
+        return currentPath;
       }
 
-      numSolutionMoves++;
-      console.log(queue);
-      numMovesToTest = queue.length;
+      // no solution yet, so construct new paths from the legal moves available
+      //  from currentLocation, and put them back in the queue
+      const legalMoves = this.movesMap.get(JSON.stringify(currentLocation));
+      legalMoves.forEach((move) => {
+        const newPath = [...currentPath];
+        newPath.push(move);
+        queue.push(newPath);
+      });
     }
-
-    console.log(solutions);
-    return solutions;
-  }
-
-  // need method to give paths travelled s.t. only contains paths where path[last]
-  //  is as legal move from path[last - 1], and a path is only ever added if the [next]
-  //  location has NOT already been visited (to prevent looping back)
-  updatePaths(pathsTravelled, next, visited) {
-    const newPaths = [];
-
-    pathsTravelled.forEach((path) => {
-      console.log(path);
-      const last = path[path.length - 1];
-      if (this.movesMap.get(JSON.stringify(last)).has(next)) {
-        console.log(`${next} is legal move from ${last}`);
-        const nextPath = [...path];
-        // console.log(nextPath);
-        nextPath.push(next);
-        // console.log(nextPath);
-        newPaths.push(nextPath);
-      }
-    });
-
-    return newPaths;
   }
 }
 
--- projects/07-knights-travails/testKnightMoves.mjs
+++ projects/07-knights-travails/testKnightMoves.mjs
@@ -3,4 +3,7 @@
 import KnightMoves from './knightMoves.mjs';
 
 const board = new KnightMoves();
-board.knightMoves([0, 0], [3, 3]);
+console.log(board.knightMoves([0, 0], [3, 3]));
+console.log(board.knightMoves([3, 3], [0, 0]));
+console.log(board.knightMoves([3, 3], [4, 3]));
+console.log(board.knightMoves([0, 0], [7, 7]));
